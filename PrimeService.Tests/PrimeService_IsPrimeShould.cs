9a9c3854f1bd1c52c1d06d46a01fe4de15a0659e
admin <yohanesrinaldy127@gmail.com>
2022-01-06 12:29:24 +0700

    test

--- PrimeService.Tests/PrimeService_IsPrimeShould.cs
+++ PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -6,11 +6,43 @@ namespace Prime.UnitTests.Services
     public class PrimeService_IsPrimeShould
     {
         [Fact]
+        public void IsPrime_InputIs1_ReturnSuccess()
+        {
+            var primeService = new PrimeService();
+            bool result = primeService.IsPrime(3);
+			Assert.True(result,"Success");
+        }
+		
+		[Fact]
         public void IsPrime_InputIs1_ReturnFalse()
         {
             var primeService = new PrimeService();
             bool result = primeService.IsPrime(3);
 			Assert.False(result,"Fail");
+        }
+		
+		[Fact]
+        public void IsPrime_InputIs1_ReturnFalse2()
+        {
+            var primeService = new PrimeService();
+            bool result = primeService.IsPrime(3);
+			Assert.False(result,"Fail");
+        }
+		
+		[Fact]
+        public void IsPrime_InputIs1_ReturnSuccess2()
+        {
+            var primeService = new PrimeService();
+            bool result = primeService.IsPrime(3);
+			Assert.True(result,"Success");
+        }
+		
+		[Fact]
+        public void IsPrime_InputIs1_ReturnSuccess3()
+        {
+            var primeService = new PrimeService();
+            bool result = primeService.IsPrime(3);
+			Assert.True(result,"Success");
         }
     }
 }
